f33d1d5d7f11254c9a0d2c579f0bf1e4092c2f4b
Sideron <guardian2012cave@gmail.com>
2024-12-01 16:31:39 -0500

    conexion DB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import express from "express"
 import cors from "cors"
+import { sequelize } from "./database.js"
+import { Juego } from "./models/juego.js"
 
 const app = express()
+
+await sequelize.sync({force:false})
+
+app.get('/juegos', async (req, res) => {
+    const juegos = await Juego.findAll()
+    res.send(juegos)
+})
 
 app.listen(3001,()=>{
     console.log("Conectado en el puerto 3001")
