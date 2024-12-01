a8458df0fe4ee7a0724bb20c70fb0726e9c36c4f
Sideron <guardian2012cave@gmail.com>
2024-11-30 20:16:14 -0500

    actualizar con tu contraseña de psotgres

--- src/database.js
+++ src/database.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 
+// Actualizar con la contraseña de tu db
 export const sequelize = new Sequelize('projectdb','postgres','bruno',{
     host: 'localhost',
     dialect: 'postgres'
