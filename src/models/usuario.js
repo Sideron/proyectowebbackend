75dd51b881e0895cdd0414030f8fcd60fda83190
Sideron <guardian2012cave@gmail.com>
2024-11-30 20:57:53 -0500

    hay chamba, mucha chamba

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,5 +1,7 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../database.js";
+import { Cliente } from "./cliente.js";
+import { Administrador } from "./admin.js";
 
 export const Usuario = sequelize.define('usuarios',{
     id:{
@@ -14,3 +16,23 @@ export const Usuario = sequelize.define('usuarios',{
         type: DataTypes.STRING
     }
 },{timestamps: false})
+
+Usuario.hasMany(Cliente, {
+    foreignKey: 'usuario_id',
+    sourceKey: 'id'
+})
+
+Cliente.belongsTo(Usuario, {
+    foreignKey: 'usuario_id',
+    targetKey: 'id'
+})
+
+Usuario.hasMany(Administrador, {
+    foreignKey: 'usuario_id',
+    sourceKey: 'id'
+})
+
+Administrador.belongsTo(Usuario, {
+    foreignKey: 'usuario_id',
+    targetKey: 'id'
+})
