e99487e82bedf0fbeee52b04ba33fc168d70b3e6
Sideron <guardian2012cave@gmail.com>
2024-12-02 00:26:37 -0500

    Update login.js

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -34,6 +34,27 @@ const login = (app) => {
             resp.status(400).send(dataOutput)
         }
     })
+    app.get("/login/:id",async (req, resp) => {
+        const id = req.params.id
+        const usuarios = await Usuario.findAll({
+            where:{
+                id
+            }
+        })
+        if (usuarios.length > 0) {
+            // Login exitoso
+            const dataOutput = {
+                error : ""
+            }
+            resp.send(usuarios[0])
+        }else {
+            // Error login
+            const dataOutput = {
+                error : "Error en el login."
+            }
+            resp.status(400).send(dataOutput)
+        }
+    })
 }
 
 export default login
